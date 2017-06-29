a4e5599bcf9ec31fddd2ce061261bd398462e8d2
Paul Hirch <Paul-Jasper@hotmail.de>
2017-06-29 23:27:41 +0200

    Add files via upload

--- ColorPicker.js
+++ ColorPicker.js
@@ -17,37 +17,42 @@ class ColorPicker{
       this.htmlLeverY;
       this.htmlLeverX = [];
       this.htmlTxtX = [];
+      this.htmlLabelX = [];
       this.buildHTML(id);
       this.addEvents();
       this.startRender();
 
+      this.fontColor
+      this.inputColor
+      this.leverColor
+      this.backColor
+      this.frameColor
+      this.border
+      this.font
+
     }
     buildHTML(id){
       this.htmlDiv = document.createElement('div');
-      this.htmlDiv.className ="color-div";
+      this.htmlDiv.style.position = "absolute";
       this.htmlDiv.style.display = "none";
-      this.htmlDiv.style.left = "100px";
-      this.htmlDiv.style.top = "100px";
       this.htmlDiv.style.width = "470px";
       this.htmlDiv.style.height = "210px";
       document.body.appendChild(this.htmlDiv);
 
-      let obj;
-      this.htmlFrame = document.createElement('label');
-      this.htmlFrame.className ="color-frame";
-      this.htmlFrame.innerHTML  = "ColorPicker v0.1";
-      this.htmlFrame.style.left = "-1px";this.htmlFrame.style.top = "-1px";
+      this.htmlFrame = document.createElement('div');
+      this.htmlFrame.style.position = "absolute";
+      this.htmlFrame.style.left = "0px";this.htmlFrame.style.top = "0px";
       this.htmlFrame.style.width = "470px";this.htmlFrame.style.height = "20px";
       this.htmlDiv.appendChild(this.htmlFrame);
 
       this.htmlCanvas = document.createElement('canvas');
-      this.htmlCanvas.className ="color-pick";
+      this.htmlCanvas.style.position = "absolute";
       this.htmlCanvas.style.left = "10px";this.htmlCanvas.style.top = "30px";
       this.htmlCanvas.width = 170;this.htmlCanvas.height = 170;
       this.htmlDiv.appendChild(this.htmlCanvas);
 
       this.htmlLeverY = document.createElement('canvas');
-      this.htmlLeverY.className ="color-lever";
+      this.htmlLeverY.style.position = "absolute";
       this.htmlLeverY.style.left = "190px";this.htmlLeverY.style.top = "30px";
       this.htmlLeverY.width = 20;this.htmlLeverY.height = 170;
       this.htmlDiv.appendChild(this.htmlLeverY);
@@ -55,27 +60,61 @@ class ColorPicker{
       let identifier = ["H","S","V","R","G","B"];
       for(let i=1;i<=6;i++){
 
-      obj = document.createElement('label');
-      obj.className ="color-txt";
-      obj.innerHTML  = identifier[i-1]+":";
-      obj.style.left = "240px";obj.style.top = (30*i)+"px";
-      obj.style.width = "20px";obj.style.height = "20px";
-      this.htmlDiv.appendChild(obj);
+        this.htmlLabelX[i-1] = document.createElement('label');
+        this.htmlLabelX[i-1].style.position = "absolute";
+        this.htmlLabelX[i-1].innerHTML  = identifier[i-1]+":";
+        this.htmlLabelX[i-1].style.left = "240px";this.htmlLabelX[i-1].style.top = (30*i)+"px";
+        this.htmlLabelX[i-1].style.width = "20px";this.htmlLabelX[i-1].style.height = "20px";
+        this.htmlDiv.appendChild(this.htmlLabelX[i-1]);
 
-      this.htmlLeverX[i-1] = document.createElement('canvas');
-      this.htmlLeverX[i-1].className ="color-lever";
-      this.htmlLeverX[i-1].style.left = "270px";this.htmlLeverX[i-1].style.top = 30*i+"px";
-      this.htmlLeverX[i-1].width = 140;this.htmlLeverX[i-1].height = 20;
-      this.htmlDiv.appendChild(this.htmlLeverX[i-1]);
+        this.htmlLeverX[i-1] = document.createElement('canvas');
+        this.htmlLeverX[i-1].style.position = "absolute";
+        this.htmlLeverX[i-1].style.left = "270px";this.htmlLeverX[i-1].style.top = 30*i+"px";
+        this.htmlLeverX[i-1].width = 140;this.htmlLeverX[i-1].height = 20;
+        this.htmlDiv.appendChild(this.htmlLeverX[i-1]);
 
 
-      this.htmlTxtX[i-1] = document.createElement('input');
-      this.htmlTxtX[i-1].className ="color-input";
-      this.htmlTxtX[i-1].style.left = "420px";this.htmlTxtX[i-1].style.top = (30*i)+"px";
-      this.htmlTxtX[i-1].style.width = "40px";this.htmlTxtX[i-1].style.height = "20px";
-      this.htmlDiv.appendChild(this.htmlTxtX[i-1]);
+        this.htmlTxtX[i-1] = document.createElement('input');
+        this.htmlTxtX[i-1].style.position = "absolute";
+        this.htmlTxtX[i-1].style.left = "420px";this.htmlTxtX[i-1].style.top = (30*i)+"px";
+        this.htmlTxtX[i-1].style.width = "40px";this.htmlTxtX[i-1].style.height = "20px";
+        this.htmlDiv.appendChild(this.htmlTxtX[i-1]);
       }
+    }
+    setStyle(fontColor,inputColor,leverColor,backColor,frameColor,border,font){
+      this.fontColor = fontColor;
+      this.inputColor = inputColor;
+      this.leverColor = leverColor;
+      this.backColor = backColor;
+      this.frameColor = frameColor;
+      this.border = border
+      this.font = font
 
+      this.applyStyle(this.htmlDiv,2)
+      this.applyStyle(this.htmlFrame,3)
+      this.applyStyle(this.htmlCanvas,1)
+      this.applyStyle(this.htmlLeverY,1)
+      for(let i=1;i<=6;i++){
+        this.applyStyle(this.htmlLabelX[i-1],0)
+        this.applyStyle(this.htmlLeverX[i-1],1)
+        this.applyStyle(this.htmlTxtX[i-1],1)
+      }
+    }
+    applyStyle(obj,style){
+      obj.style.position = "absolute";
+      obj.style.color = this.fontColor;
+      obj.style.font = this.font;
+      obj.style.msUserSelect = "none";
+      obj.style.webkitUserSelect = "none";
+      obj.style.mozUserSelect = "none";
+      obj.style.userSelect = "none";
+      if(style!==0) obj.style.border = this.border;
+      switch(style)
+      {
+        case 1:obj.style.background = this.inputColor;break;
+        case 2:obj.style.background = this.backColor;break;
+        case 3:obj.style.background = this.frameColor;break;
+      }
 
     }
     addEvents(){
@@ -95,9 +134,44 @@ class ColorPicker{
       this.htmlLeverX[4].addEventListener('mousedown', (e) => {this.aktiveElementObj=obj;this.aktiveElement=8;});
       this.htmlLeverX[5].addEventListener('mousedown', (e) => {this.aktiveElementObj=obj;this.aktiveElement=9;});
 
+      this.htmlTxtX[0].addEventListener('change', (e) => {
+        if(this.htmlTxtX[0].value<0)this.htmlTxtX[0].value=0;
+        else if(this.htmlTxtX[0].value>360)this.htmlTxtX[0].value=360;
+        this.colorHSV[0] = this.htmlTxtX[0].value/360
+      })
+      this.htmlTxtX[1].addEventListener('change', (e) => {
+        if(this.htmlTxtX[1].value<0)this.htmlTxtX[1].value=0;
+        else if(this.htmlTxtX[1].value>100)this.htmlTxtX[1].value=100;
+        this.colorHSV[1] = this.htmlTxtX[1].value/100
+      })
+      this.htmlTxtX[2].addEventListener('change', (e) => {
+        if(this.htmlTxtX[2].value<0)this.htmlTxtX[2].value=0;
+        else if(this.htmlTxtX[2].value>100)this.htmlTxtX[2].value=100;
+        this.colorHSV[2] = this.htmlTxtX[2].value/100
+      })
+      this.htmlTxtX[3].addEventListener('change', (e) => {
+        if(this.htmlTxtX[3].value<0)this.htmlTxtX[3].value=0;
+        else if(this.htmlTxtX[3].value>255)this.htmlTxtX[3].value=255;
+        this.colorRGB[0] = this.htmlTxtX[3].value; 
+        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+      })
+      this.htmlTxtX[4].addEventListener('change', (e) => {
+        if(this.htmlTxtX[4].value<0)this.htmlTxtX[4].value=0;
+        else if(this.htmlTxtX[4].value>255)this.htmlTxtX[4].value=255;
+        this.colorRGB[1] = this.htmlTxtX[4].value; 
+        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+      })
+      this.htmlTxtX[5].addEventListener('change', (e) => {
+        if(this.htmlTxtX[5].value<0)this.htmlTxtX[5].value=0;
+        else if(this.htmlTxtX[5].value>255)this.htmlTxtX[5].value=255;
+        this.colorRGB[2] = this.htmlTxtX[5].value; 
+        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+      })
+
 
       document.addEventListener('mousemove', (e) => {
         if (this.mouseDown===true){
+          //let oldColorRGB = colorRGB;
           let click = null;
           switch(this.aktiveElement)
           {
@@ -110,49 +184,48 @@ class ColorPicker{
             case 2:
               click = this.click(this.htmlCanvas,e,-5,-5,160,160);
               this.colorHSV[2] = click[0];this.colorHSV[1] = 1-click[1];
-			  this.colorRGB = this.createRGBfromHSV(this.colorHSV);
+			        this.colorRGB = this.createRGBfromHSV(this.colorHSV);
             break;
             case 3:
               click = this.click(this.htmlLeverY,e,0,0,1,170);
               this.colorHSV[0] = 1-click[1];
-			  this.colorRGB = this.createRGBfromHSV(this.colorHSV);
+			        this.colorRGB = this.createRGBfromHSV(this.colorHSV);
             break;
             case 4:
               click = this.click(this.htmlLeverX[0],e,0,0,140,1);
               this.colorHSV[0] = click[0];
-			  this.colorRGB = this.createRGBfromHSV(this.colorHSV);
+			        this.colorRGB = this.createRGBfromHSV(this.colorHSV);
             break;
             case 5:
               click = this.click(this.htmlLeverX[1],e,0,0,140,1);
               this.colorHSV[1] = click[0];
-			  this.colorRGB = this.createRGBfromHSV(this.colorHSV);
+			        this.colorRGB = this.createRGBfromHSV(this.colorHSV);
             break;
             case 6:
               click = this.click(this.htmlLeverX[2],e,0,0,140,1);
               this.colorHSV[2] = click[0];
-			  this.colorRGB = this.createRGBfromHSV(this.colorHSV);
+			        this.colorRGB = this.createRGBfromHSV(this.colorHSV);
             break;
             case 7:
               click = this.click(this.htmlLeverX[3],e,0,0,140,1);
               this.colorRGB[0] = click[0]*255;
-			  this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+			        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
             break;
             case 8:
               click = this.click(this.htmlLeverX[4],e,0,0,140,1);
               this.colorRGB[1] = click[0]*255;
-			  this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+			        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
             break;
             case 9:
               click = this.click(this.htmlLeverX[5],e,0,0,140,1);
               this.colorRGB[2] = click[0]*255;
-			  this.colorHSV = this.createHSVfromRGB(this.colorRGB);
+			        this.colorHSV = this.createHSVfromRGB(this.colorRGB);
             break;
           }
+          if (this.aktiveElement!==0)this.UpdateTxtX();
         }
       });
     //FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUNNNNNNNNNNNNNNNNNNNNNNNNN
-    }
-    setLevler(lever1,lever2,lever3,lever4,lever5,lever6){
     }
   //--Logik---------------------------------------------------------------------------------------------------------------------------------
     show(posX,posY){
@@ -172,6 +245,14 @@ class ColorPicker{
       if (posX<0)posX=0;if (posY<0)posY=0;
       if (posX>1)posX=1;if (posY>1)posY=1;
       return [posX,posY];
+    }
+    UpdateTxtX(){
+      this.htmlTxtX[0].value = (this.colorHSV[0]*360)|0;
+      this.htmlTxtX[1].value = (this.colorHSV[1]*100)|0;
+      this.htmlTxtX[2].value = (this.colorHSV[2]*100)|0;
+      this.htmlTxtX[3].value = this.colorRGB[0]|0;
+      this.htmlTxtX[4].value = this.colorRGB[1]|0;
+      this.htmlTxtX[5].value = this.colorRGB[2]|0;
     }
   //--ColorOperators---------------------------------------------------------------------------------------------------------------------------------
     createRGBfromH(value,max) {
@@ -424,7 +505,7 @@ class ColorPicker{
     render(){
       let context;let SmoothingEnabled = true;
       let resolution = 42;
-      let fillStyle = "rgba(75,75,75,1)"; 
+      let fillStyle = this.leverColor; 
 	
 
       context = this.clearCanvas(this.htmlCanvas,SmoothingEnabled,fillStyle)
@@ -441,31 +522,25 @@ class ColorPicker{
       context = this.clearCanvas(this.htmlLeverX[0],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorHSV[0])*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX0(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[0].value = (this.colorHSV[0]*360)|0;
 
       context = this.clearCanvas(this.htmlLeverX[1],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorHSV[1])*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX1(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[1].value = (this.colorHSV[1]*100)|0;
 
       context = this.clearCanvas(this.htmlLeverX[2],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorHSV[2])*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX2(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[2].value = (this.colorHSV[2]*100)|0;
 
       context = this.clearCanvas(this.htmlLeverX[3],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorRGB[0]/255)*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX3(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[3].value = this.colorRGB[0]|0;
 
       context = this.clearCanvas(this.htmlLeverX[4],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorRGB[1]/255)*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX4(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[4].value = this.colorRGB[1]|0;
       
       context = this.clearCanvas(this.htmlLeverX[5],SmoothingEnabled,fillStyle)
       context.fillRect((this.colorRGB[2]/255)*140-5,0, 10, 20);
       context.drawImage(this.drawColorLeverX5(resolution), 0, 5, 140, 10);
-      this.htmlTxtX[5].value = this.colorRGB[2]|0;
     }
 }
--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function main(){
-	console.log("Start");
 	let Test = new ColorPicker("1");
+             //(   fontColor,     		inputColor,					leverColor,			backColor,							frameColor,					border,										font                       )
+	Test.setStyle("rgb(230,230,230)","rgba(20,20,20,0.95)","rgb(75,75,75)","rgba(31,31,31,0.95)","rgba(18,18,18,0.95)","1px solid rgb(42,42,42)","13px Arial, Times, serif")
 	Test.show(500,200);
-	// 	let Test2 = new ColorPicker("2");
-	// Test2.show(600,200);
+
+
+
+
+	let Test2 = new ColorPicker("1");
+	Test2.setStyle("rgb(0,0,0)","rgba(200,200,220,0.95)","rgb(80,80,255)","rgba(180,180,240,0.95)","rgba(100,100,255,0.95)","1px rgb(0,0,0)","13px Arial, Times, serif")
+	Test2.show(700,300);
+	let Test3 = new ColorPicker("1");
+	Test3.setStyle("rgb(255,150,0)","rgba(10,20,0,0.95)","rgb(255,150,0)","rgba(20,30,10,0.95)","rgba(10,20,0,0.95)","1px solid rgb(100,102,0)","15px Consolas, Times, serif")
+	Test3.show(600,100);
+
 }
 main();
